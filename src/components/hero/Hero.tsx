26a3df3cfd01315eb0d449ee4f0402411c3304f1
isiagi <isiagigeofrey0@gmail.com>
2023-04-28 14:22:10 +0300

    change title

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -7,7 +7,7 @@ function Hero() {
     <div className="hero__container">
       <div className="middle hero__wrapper">
         <div className="hero__first">
-        <h1 className="hero__h1">I am Geofrey Isiagi</h1>
+        <h1 className="hero__h1">I am Lwanga Samson</h1>
           <h1 className="hero__h1">Am a Developer</h1>
           <p className="hero__para">
             I do fullstack Web, Cloud Development with Proficience in
